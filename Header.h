80969b1926dcbd59dac843b29072df076e833ecc
xebra24 <66282217+xebra24@users.noreply.github.com>
2020-06-03 19:57:06 +1000

    Update head.h

--- Header.h
+++ Header.h
@@ -4,10 +4,14 @@
 //VARIABLES
 //Vectors for 10 people, pancakes
 extern std::vector<int> person;
-extern std::vector<int> pancakes;
+extern std::vector<int> num_eaten_pancakes;
+
 extern int most_pancakes;
+extern int least_pancakes;
 extern int person_who_ate_most;
+extern int person_who_ate_least;
 
 //FUNCTIONS
 void get_nums_of_pancakes();
 int who_ate_most();
+int who_ate_least();
