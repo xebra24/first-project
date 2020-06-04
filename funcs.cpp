1d3395e01eb83b79c99820f9714a6307b4703ef7
xebra24 <66282217+xebra24@users.noreply.github.com>
2020-06-04 12:51:53 +1000

    Update funcs.cpp
    Added review_entries funtion

--- funcs.cpp
+++ funcs.cpp
@@ -37,3 +37,46 @@ int who_ate_least() {
     }
     return person_who_ate_least;
 }
+
+void review_entries() {
+    //Ask user if they want to review entries
+    std::string yes_or_no = "";
+    std::cout << "Would you like to review your entries? Y/n: ";
+    std::cin >> yes_or_no;
+
+    //If answer is yes, we output all the entries, then check if it is correct.
+    if (yes_or_no == "y" || yes_or_no == "Y") {
+        for (int i = 0; i < 10; i++) {
+            std::cout << "Person " << person[i] << " ate " << num_eaten_pancakes[i] << "\n";
+        }
+        std::cout << "\n" << "Is this correct? Y/n: ";
+        std::cin >> yes_or_no;
+        
+        //If it is correct, we finish the program.
+        if (yes_or_no == "Y" || yes_or_no == "y") {
+            std::cout << "Goodbye!";
+        }
+
+        //If it is not correct, we do the whole thing again.
+        else
+            get_nums_of_pancakes();
+            display_person_who_ate_most();
+            display_person_who_ate_least();
+            review_entries();
+    }
+
+    else
+        return;
+}
+
+void display_person_who_ate_most() {
+    person_who_ate_most = who_ate_most();
+    std::cout << "Person " << who_ate_most() << " ate the most pancakes with " 
+        << num_eaten_pancakes[(__int64)person_who_ate_most - 1] << " eaten." << std::endl;
+}
+
+void display_person_who_ate_least() {
+    person_who_ate_least = who_ate_least();
+    std::cout << "Person " << who_ate_least() << " ate the least pancakes with " 
+        << num_eaten_pancakes[(__int64)person_who_ate_least - 1] << " eaten." << std::endl;
+}
